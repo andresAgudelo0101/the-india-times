c675aedd228961bb013d84cd8a557d0dcfaf4fd5
andres <echeverry11298@gmail.com>
2022-05-20 16:17:16 -0500

    queda un largo camino por recorrer

---
+++ components/Sports.js
@@ -0,0 +1,10 @@
+function Sports() {
+    return (
+        <div id="sports">
+            hila
+            <a href="#home">inicio</a>
+        </div>
+    )
+}
+
+export default Sports;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(<>
+  <Component {...pageProps} />
+  <footer >
+  <a
+    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Powered by{' '}
+    <span >
+      <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+    </span>
+  </a>
+</footer>
+</>)
 }
 
 export default MyApp
